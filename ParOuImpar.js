3bd9d6e058af42b30cf169d69b2e2e4625c1f744
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-21 16:36:28 -0300

    Update ParOuImpar.js

--- ParOuImpar.js
+++ ParOuImpar.js
@@ -4,7 +4,7 @@ if(num%2 != 0){
     alert('O numero '+num+' é impar');
 }
 else if(num == 0){
-    alert('Seu numero é 0');
+    alert('O numero escolhido é 0');
 }
 else{
     alert('O numero '+num+' é par');   
