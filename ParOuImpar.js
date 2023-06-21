fa0451eae51971ad39d164e1a24fc1816fe86572
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-21 16:35:33 -0300

    Par ou Impar

---
+++ ParOuImpar.js
@@ -0,0 +1,11 @@
+alert('Programa Par ou impar')
+let num = prompt("Digite um numero");
+if(num%2 != 0){
+    alert('O numero '+num+' é impar');
+}
+else if(num == 0){
+    alert('Seu numero é 0');
+}
+else{
+    alert('O numero '+num+' é par');   
+};
