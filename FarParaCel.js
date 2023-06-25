be74b68f0f0ee727d3a0f23c32b82cb09fa5a1f0
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-25 20:35:27 -0300

    Fahrenheit para celsius

---
+++ FarParaCel.js
@@ -0,0 +1,6 @@
+function temp(num){
+    return x = Far-32*5/9;
+  }
+  let Cel = prompt("Digite a temperatura em Celsius: ");
+  let Far = temp(Cel);
+  alert(Cel + "Celsius em Fahrenheit é: " + Far);
