783cdd8dd7a56e885ff1e2733a198460a445a8f2
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-07-21 22:37:06 -0300

    Atualizar o FarParaCel.js

--- FarParaCel.js
+++ FarParaCel.js
@@ -1,6 +1,6 @@
 function temp(num){
     return x = Far-32*5/9;
   }
-  let Cel = prompt("Digite a temperatura em Celsius: ");
+  let Cel = prompt("Enter the temperature in Celsius: ");
   let Far = temp(Cel);
-  alert(Cel + "Celsius em Fahrenheit é: " + Far);
+  alert(Cel + "Celsius to Fahrenheit is: " + Far);
